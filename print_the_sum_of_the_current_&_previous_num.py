f4218d4ab500164d6750614c5886fd80417cefcd
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 04:41:32 +0800

    done

--- print_the_sum_of_the_current_&_previous_num.py
+++ print_the_sum_of_the_current_&_previous_num.py
@@ -1,10 +1,9 @@
-# #----- FUNCTION/S -----
-
 
 #----- PSEUDO CODE -----
-
+# - Getting the Loop of prev, current, and sum number
 previous_num = 0
 for i in range(10):
     sum = previous_num + i
-    if previous_num == i:
-        print("Current Number: ", i, "   Previous Number: ", previous_num, "   Sum: ", sum)
+    print("Current Number ", i,  "Previous Number: ", previous_num,  "Sum: ", sum) 
+    previous_num = i
+    

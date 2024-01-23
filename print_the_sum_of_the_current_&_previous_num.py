a9d49cb9739b805b369993fe3c38fca2f54963bb
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 04:24:15 +0800

    current num, prev num, sum, it worked

--- print_the_sum_of_the_current_&_previous_num.py
+++ print_the_sum_of_the_current_&_previous_num.py
@@ -5,4 +5,6 @@
 
 previous_num = 0
 for i in range(10):
-    print(i)
+    sum = previous_num + i
+    if previous_num == i:
+        print("Current Number: ", i, "   Previous Number: ", previous_num, "   Sum: ", sum)

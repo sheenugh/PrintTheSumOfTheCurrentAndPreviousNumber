b42cb4cf8085736b8cc3af94f725d4fb31be67e6
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 13:48:25 +0800

    spacing added

--- print_the_sum_of_the_current_&_previous_num.py
+++ print_the_sum_of_the_current_&_previous_num.py
@@ -4,6 +4,6 @@
 previous_num = 0
 for i in range(10):
     sum = previous_num + i
-    print("Current Number ", i,  "Previous Number: ", previous_num,  "Sum: ", sum) 
+    print("Current Number ", i,  " Previous Number: ", previous_num,  " Sum: ", sum) 
     previous_num = i
     
